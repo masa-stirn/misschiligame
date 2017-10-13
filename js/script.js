4f9505765f60e8251b716edb6f9b269a992d5d2e
Masa Wu <masastirn23@gmail.com>
2017-10-13 18:13:43 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+ let girl = document.querySelector(".girl");
+        let doughnut = document.querySelector(".doughnut");
+        let chocolate = document.querySelector(".chocolate");
+        let flour = document.querySelector(".flour");
+        let sugar = document.querySelector(".sugar");
+        let macaron = document.querySelector(".macaron");
+        let milk = document.querySelector(".milk");
+        let butter = document.querySelector(".butter");
+        let egg = document.querySelector(".egg");
+        let burger = document.querySelector(".burger");
+
+        function pageLoad(){
+        console.log("function: pageLoad")
+        let bgsound = document.querySelector("#bgsound");
+        //bgsound.play();
+        }
+        doughnut.addEventListener("click", pickDoughnut)
+
+function pickDoughnut(){
+    console.log("pickDoughnut");
+    girl.classList.toggle("shakeCSS");
+    button[0].classList.toggle("yellow");
+    if(char.classList.contains("shakeCSS")){
+
+    }}
