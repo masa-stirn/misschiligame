a38539e401847273e9fb1b359b1c9748c2e70982
Masa Wu <masastirn23@gmail.com>
2017-10-13 21:47:18 +0200

    girl walks to the doughnut on click, the error sound is played

--- js/script.js
+++ js/script.js
@@ -1,25 +1,34 @@
  let girl = document.querySelector(".girl");
-        let doughnut = document.querySelector(".doughnut");
-        let chocolate = document.querySelector(".chocolate");
-        let flour = document.querySelector(".flour");
-        let sugar = document.querySelector(".sugar");
-        let macaron = document.querySelector(".macaron");
-        let milk = document.querySelector(".milk");
-        let butter = document.querySelector(".butter");
-        let egg = document.querySelector(".egg");
-        let burger = document.querySelector(".burger");
+ let doughnut = document.querySelector(".doughnut");
+ let chocolate = document.querySelector(".chocolate");
+ let flour = document.querySelector(".flour");
+ let sugar = document.querySelector(".sugar");
+ let macaron = document.querySelector(".macaron");
+ let milk = document.querySelector(".milk");
+ let butter = document.querySelector(".butter");
+ let egg = document.querySelector(".egg");
+ let burger = document.querySelector(".burger");
+let chili = document.querySelector(".chili");
+let wrongAudio = document.getElementById("wrong");
 
-        function pageLoad(){
-        console.log("function: pageLoad")
-        let bgsound = document.querySelector("#bgsound");
-        //bgsound.play();
-        }
-        doughnut.addEventListener("click", pickDoughnut)
+ function pageLoad() {
+     console.log("function: pageLoad")
+     let bgsound = document.querySelector("#bgsound");
+     //bgsound.play();
+ }
+ doughnut.addEventListener("click", pickDoughnut)
 
-function pickDoughnut(){
-    console.log("pickDoughnut");
-    girl.classList.toggle("shakeCSS");
-    button[0].classList.toggle("yellow");
-    if(char.classList.contains("shakeCSS")){
+ function pickDoughnut() {
+     console.log("pickDoughnut");
+     girl.classList.add("pickDoughnut");
+     //if (girl.classList.contains("pickDoughnut")) {
+    //let wrongAudio = document.getElementById("wrong");
+    //wrongAudio.play();
+    // }
+     setTimeout(pickDoughnutDone, 2000);
+ }
+function pickDoughnutDone() {
+    wrongAudio.play();
+    chili.classList("burn");
+}
 
-    }}
