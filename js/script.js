292336961c1e7107b80397cdd00ab257ac6c4489
Masa Wu <masastirn23@gmail.com>
2017-10-14 09:16:18 +0200

    added yes, no sound, pick up and jump milk, sugar

--- js/script.js
+++ js/script.js
@@ -9,7 +9,9 @@
  let egg = document.querySelector(".egg");
  let burger = document.querySelector(".burger");
 let chili = document.querySelector(".chili");
+let chili2 = document.querySelector(".chili2");
 let wrongAudio = document.getElementById("wrong");
+let yesAudio = document.getElementById("right");
 
  function pageLoad() {
      console.log("function: pageLoad")
@@ -19,16 +21,91 @@ let wrongAudio = document.getElementById("wrong");
  doughnut.addEventListener("click", pickDoughnut)
 
  function pickDoughnut() {
-     console.log("pickDoughnut");
      girl.classList.add("pickDoughnut");
-     //if (girl.classList.contains("pickDoughnut")) {
-    //let wrongAudio = document.getElementById("wrong");
-    //wrongAudio.play();
-    // }
      setTimeout(pickDoughnutDone, 2000);
  }
 function pickDoughnutDone() {
     wrongAudio.play();
-    chili.classList("burn");
+    chili.classList.remove("chili-def");
+    chili.classList.add("hide");
+    chili2.classList.add("burnDoughnut");
+    chili2.addEventListener('animationend', hideDougnut)
+}
+
+function hideDougnut(){
+    doughnut.classList.add("hide");
+    setTimeout(chiliHome, 500);
+}
+
+function chiliHome(){
+    chili.classList.add("chili-def");
+    chili.classList.remove("hide");
+    chili2.classList.remove("burnDoughnut");
+        girl.classList.remove("pickDoughnut");
+}
+
+macaron.addEventListener("click", pickMacaron)
+
+ function pickMacaron() {
+     girl.classList.add("pickMacaron");
+     setTimeout(pickMacaronDone, 2000);
+ }
+
+function pickMacaronDone() {
+    wrongAudio.play();
+    chili.classList.remove("chili-def");
+    chili.classList.add("hide");
+    chili2.classList.add("burnMacaron");
+    chili2.addEventListener('animationend', hideMacaron)
+}
+
+function hideMacaron(){
+    macaron.classList.add("hide");
+    setTimeout(chiliHome2, 500);
+}
+
+function chiliHome2(){
+    chili.classList.add("chili-def");
+    chili.classList.remove("hide");
+    chili2.classList.remove("burnMacaron");
+     girl.classList.remove("pickMacaron");
+}
+
+milk.addEventListener("click", pickMilk)
+
+ function pickMilk() {
+     girl.classList.add("pickMilk");
+     setTimeout(pickMilkDone, 2000);
+ }
+
+function pickMilkDone() {
+    girl.classList.add("jumpMilk");
+    yesAudio.play();
+    yesAudio.addEventListener('ended', hideMilk)
+}
+
+function hideMilk(){
+    milk.classList.add("hide");
+    girl.classList.remove("jumpMilk");
+    girl.classList.remove("pickMilk");
+}
+
+sugar.addEventListener("click", pickSugar)
+
+ function pickSugar() {
+     girl.classList.add("pickSugar");
+     setTimeout(pickSugarDone, 2000);
+ }
+
+function pickSugarDone() {
+    girl.classList.add("jumpSugar");
+    yesAudio.play();
+    yesAudio.addEventListener('ended', hideSugar)
+}
+
+function hideSugar(){
+    sugar.classList.add("hide");
+        girl.classList.remove("pickSugar");
+    girl.classList.remove("jumpSugar");
 }
 
